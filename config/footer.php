8755d380be57997fa0f0a8483f49d533a7b3dc14
mjrocks30 <maulik.shrishti@gmail.com>
2022-12-02 00:49:35 +0530

    updated

---
+++ config/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer fixed-bottom">
+    <strong>Copyright &copy; <?php echo date('Y');?> 
+    <a href="./">MEDICAL CARD</a>.</strong> All rights reserved.
+    <div class="float-right d-sm-block">
+      MEDICAL CARD 
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
